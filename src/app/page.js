e6dcbf5d34e70475605cffbf93f83931bff6209e
songexile <therunningbeta@gmail.com>
2024-02-25 20:13:33 +1300

    ADDED cool table

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main
-      className="flex min-h-screen flex-col items-center justify-between h-[48rem] "
+      className="flex min-h-screen flex-col items-center justify-between h-full sm:h-[48rem] "
       style={{ backgroundImage: "url('/background-img.jpg')" }}
     >
       <div className="mx-64 w-5/6 lg:w-4/6 xl:w-4/6 shadow-xl mt-4">
@@ -22,9 +22,9 @@ export default function Home() {
         ></div>
         <div
           style={{ backgroundImage: "url('/header-top.jpg')" }}
-          className="bg-cover bg-center h-4 w-full flex  "
+          className="bg-cover bg-center h-8 w-full flex  "
         >
-          <div className="w-full h-full  flex items-center gap-16 justify-start text-gray-800 text-sm">
+          <div className="w-full h-full  flex items-center gap-x-4 justify-start text-gray-800 text-sm">
             <h1>About</h1>
             <h1>FAQ</h1>
             <h1>Lifestyle</h1>
@@ -34,13 +34,52 @@ export default function Home() {
         <div className="h-3 w-full bg-gradient-to-t from-black to-gray-500 border border-b-1 border-t-0 border-white"></div>
         {/* Container looking thing */}
         <div className="bg-white h-[35rem] flex">
-          <div className=" h-full bg-blue-500 w-8/12 flex"></div>
+          {/* Left side of screen div */}
+          <div className=" h-full bg-blue-500 w-8/12 flex flex-col items-center  justify-start">
+            <div className="w-52 h-28  mt-16  rounded-t-lg shadow-xl   flex items-center justify-start flex-col">
+              <div className=" blue-gradient text-white w-full border-b-2 rounded-t-lg  h-6 flex items-center justify-center text-sm">
+                <h1>Products and Services</h1>
+              </div>
+              <div className="h-1/6 bg-blue-400 w-full border flex items-center justify-start">
+                <div className="w-1/12 h-full blue-gradient"></div>
+                <div className="w-full h-full pink-gradient"></div>
+              </div>
+              <div className="h-1/6 bg-blue-400 w-full border flex items-center justify-start">
+                <div className="w-1/12 h-full blue-gradient"></div>
+                <div className="w-full h-full pink-gradient"></div>
+              </div>
+              <div className="h-1/6 bg-blue-400 w-full border flex items-center justify-start">
+                <div className="w-1/12 h-full blue-gradient"></div>
+                <div className="w-full h-full pink-gradient"></div>
+              </div>
+              <div className="h-1/6 bg-blue-400 w-full border flex items-center justify-start">
+                <div className="w-1/12 h-full blue-gradient"></div>
+                <div className="w-full h-full pink-gradient"></div>
+              </div>
+              <div className="h-1/6 bg-blue-400 w-full border flex items-center justify-start">
+                <div className="w-1/12 h-full blue-gradient"></div>
+                <div className="w-full h-full pink-gradient"></div>
+              </div>
+              <div className="h-2 bg-white w-full border flex items-center justify-start"></div>
+              <div className="w-full h-2 pink-gradient rounded-b-xl shadow-xl"></div>
+            </div>
+            <div className="w-52 h-24  mt-16 rounded-t-lg   bg-red-500"></div>
+            <svg
+              width="300"
+              height="170"
+              xmlns="http://www.w3.org/2000/svg"
+            ></svg>
+          </div>
+          {/* Right side screen div */}
           <div
-            className=" h-full bg-blue-500 w-full"
+            className=" h-full bg-white w-full flex flex-col items-center justify-start"
             style={{
               backgroundImage: "url('/web_assets/paper_background-0000.jpg')",
             }}
-          ></div>
+          >
+            <div className="w-8/12 h-52 rounded-md mt-24 bg-red-500"></div>
+            <div className="w-8/12 h-24 rounded-md mt-8 bg-red-500"></div>
+          </div>
         </div>
       </div>
     </main>
