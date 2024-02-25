9a1ffc8881fbcebeae3826bfa1ed14873edc7482
songexile <therunningbeta@gmail.com>
2024-02-25 20:38:42 +1300

    set up boiler plate for other page

--- src/app/page.js
+++ src/app/page.js
@@ -35,35 +35,63 @@ export default function Home() {
         {/* Container looking thing */}
         <div className="bg-white h-[35rem] flex">
           {/* Left side of screen div */}
-          <div className=" h-full bg-blue-500 w-8/12 flex flex-col items-center  justify-start">
-            <div className="w-52 h-28  mt-16  rounded-t-lg shadow-xl   flex items-center justify-start flex-col">
-              <div className=" blue-gradient text-white w-full border-b-2 rounded-t-lg  h-6 flex items-center justify-center text-sm">
-                <h1>Products and Services</h1>
+          <div
+            style={{ backgroundImage: "url('web_assets/MBK1.jpg')" }}
+            className=" h-full bg-blue-400  w-8/12 flex flex-col items-center  justify-start"
+          >
+            <div className="w-52 h-FULL  mt-16 text-white  rounded-t-lg shadow-xl   flex items-center justify-start flex-col">
+              <div className=" blue-gradient  text-white w-full border-b-2 rounded-t-lg  h-6 flex items-center justify-center text-sm">
+                <h1>Products </h1>
               </div>
-              <div className="h-1/6 bg-blue-400 w-full border flex items-center justify-start">
+              <div className="h-1/6  w-full border flex items-center justify-start">
                 <div className="w-1/12 h-full blue-gradient"></div>
-                <div className="w-full h-full pink-gradient"></div>
+                <div className="w-full h-full pink-gradient flex items-center justify-start text-sm">
+                  <a href="https://www.google.com" target="_blank">
+                    <h1>RX7</h1>
+                  </a>
+                </div>
               </div>
-              <div className="h-1/6 bg-blue-400 w-full border flex items-center justify-start">
+              <div className="h-1/6  w-full border flex items-center justify-start">
                 <div className="w-1/12 h-full blue-gradient"></div>
-                <div className="w-full h-full pink-gradient"></div>
+                <div className="w-full h-full pink-gradient flex items-center justify-start text-sm">
+                  <a href="https://www.google.com" target="_blank">
+                    <h1>EK CIVIC</h1>
+                  </a>
+                </div>
               </div>
-              <div className="h-1/6 bg-blue-400 w-full border flex items-center justify-start">
+              <div className="h-1/6  w-full border flex items-center justify-start">
                 <div className="w-1/12 h-full blue-gradient"></div>
-                <div className="w-full h-full pink-gradient"></div>
+                <div className="w-full h-full pink-gradient flex items-center justify-start text-sm">
+                  <a href="https://www.google.com" target="_blank">
+                    <h1>AKL</h1>
+                  </a>
+                </div>
               </div>
-              <div className="h-1/6 bg-blue-400 w-full border flex items-center justify-start">
-                <div className="w-1/12 h-full blue-gradient"></div>
-                <div className="w-full h-full pink-gradient"></div>
-              </div>
-              <div className="h-1/6 bg-blue-400 w-full border flex items-center justify-start">
-                <div className="w-1/12 h-full blue-gradient"></div>
-                <div className="w-full h-full pink-gradient"></div>
-              </div>
+
               <div className="h-2 bg-white w-full border flex items-center justify-start"></div>
               <div className="w-full h-2 pink-gradient rounded-b-xl shadow-xl"></div>
             </div>
-            <div className="w-52 h-24  mt-16 rounded-t-lg   bg-red-500"></div>
+            <div className="w-52 h-32   flex-col flex items-center justify-start  mt-16  rounded-t-lg   pink-gradient">
+              <div className=" blue-gradient  text-white w-full border-b-2 rounded-t-lg  h-6 flex items-center justify-center text-sm">
+                <h1>MP3 TrakZ </h1>
+              </div>
+              <Image
+                className="rounded-t-xl shadow-2xl mt-2 mb-4 "
+                src="/web_assets/Kriss2.jpg"
+                alt="MBK1"
+                width={100}
+                height={100}
+              />
+              <div className=" blue-gradient  text-white w-full border-b-2  h-4 flex items-center justify-start text-sm">
+                <h1>MADE </h1>
+              </div>
+              <div className=" blue-gradient  text-white w-full border-b-2  h-4 flex items-center justify-start text-sm">
+                <h1>BY </h1>
+              </div>
+              <div className=" blue-gradient  text-white w-full border-b-2  h-4 flex items-center justify-start text-sm">
+                <h1>GITHUB LINK </h1>
+              </div>
+            </div>
             <svg
               width="300"
               height="170"
