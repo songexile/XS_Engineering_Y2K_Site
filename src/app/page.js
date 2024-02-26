b474e10d399549f898edd1f39c204ea64a97d1bb
songexile <therunningbeta@gmail.com>
2024-02-27 08:49:13 +1300

    commit latest changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,65 @@
+"use client";
+
 import Layout from "@/components/layouts/layout";
 import Image from "next/image";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
 export default function Home() {
+  const container = useRef();
+  useGSAP(
+    () => {
+      const t1 = gsap.timeline({ repeat: -1, repeatDelay: 1 });
+
+      gsap.to("#products", { x: 10 }, {});
+      gsap.to("#row-1", { x: 10 }, { delay: 5 }); // <-- automatically reverted
+    },
+    { scope: container }
+  ); // <-- scope is for selector text (optional)
+
   return (
     <Layout>
       <div
+        id="mbk"
         style={{ backgroundImage: "url('web_assets/MBK1.jpg')" }}
         className=" h-full bg-blue-400  w-full sm:w-8/12 flex flex-col items-center  justify-start"
+        ref={container}
       >
         <div className="w-52 h-FULL  mt-16 text-white  rounded-t-lg shadow-xl   flex items-center justify-start flex-col">
-          <div className=" blue-gradient  text-white w-full border-b-2 rounded-t-lg  h-6 flex items-center justify-center text-sm">
+          <div
+            id="products"
+            className=" blue-gradient  text-white w-full border-b-2 rounded-t-lg  h-6 flex items-center justify-center text-sm"
+          >
             <h1>Products </h1>
           </div>
-          <div className="h-1/6  w-full border flex items-center justify-start">
-            <div className="w-1/12 h-full blue-gradient"></div>
+          <div
+            id="row-1"
+            className="h-1/6  w-full border flex items-center justify-start"
+          >
+            <div className="w-1/12 h-full pink-gradient"></div>
             <div className="w-full h-full pink-gradient flex items-center justify-start text-sm">
               <a href="https://www.google.com" target="_blank">
                 <h1>RX7</h1>
               </a>
             </div>
           </div>
-          <div className="h-1/6  w-full border flex items-center justify-start">
-            <div className="w-1/12 h-full blue-gradient"></div>
+          <div
+            id="row-2"
+            className="h-1/6  w-full border flex items-center justify-start"
+          >
+            <div className="w-1/12 h-full pink-gradient"></div>
             <div className="w-full h-full pink-gradient flex items-center justify-start text-sm">
               <a href="https://www.google.com" target="_blank">
                 <h1>EK CIVIC</h1>
               </a>
             </div>
           </div>
-          <div className="h-1/6  w-full border flex items-center justify-start">
-            <div className="w-1/12 h-full blue-gradient"></div>
+          <div
+            id="row-3"
+            className="h-1/6  w-full border flex items-center justify-start"
+          >
+            <div className="w-1/12 h-full pink-gradient"></div>
             <div className="w-full h-full pink-gradient flex items-center justify-start text-sm">
               <a href="https://www.google.com" target="_blank">
                 <h1>AKL</h1>
